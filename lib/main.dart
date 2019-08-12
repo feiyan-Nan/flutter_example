b055cb01399324eb9020acb27951a946c4b3c8a8
liguigong <liguigong@cogo.club>
2019-08-12 11:57:55 +0800

    '制作完成'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import './bottom_appBar_demo.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,11 +11,9 @@ class MyApp extends StatelessWidget {
         title: '百姓生活+',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-            primaryColor: Colors.pink
+            primarySwatch: Colors.lightBlue
         ),
-        home: Center(
-          child: Text('内容'),
-        ),
+        home: BottomAppBarDemo()
       ),
     );
   }
